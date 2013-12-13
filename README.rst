63cae02c394c1966a8d57c7d8f5f8c3d55a8e8a7
Joshua Ma <me@joshma.com>
2013-12-13 15:58:48 -0800

    Fix doc, README in proper RST

--- README.rst
+++ README.rst
@@ -1,46 +1,42 @@
-# clustalo-python
+clustalo-python
+===============
 
 This is just a simiple Python wrapper around Clustal Omega
 (http://www.clustal.org/omega/), used internally at Benchling but casually open
-source, if it helps anybody. Also available via
+source, if it helps anybody. Also available via::
 
-```
-pip install clustalo
-```
+  pip install clustalo
 
 Requires `libclustalo` installed, version 1.2.0. On Linux, it's recommended you build and
-install it yourself:
+install it yourself::
 
-```
-cd clustal-omega-1.2.0
-./configure --with-pic --with-openmp
-make && sudo make install
-```
+  cd clustal-omega-1.2.0
+  ./configure --with-pic --with-openmp
+  make && sudo make install
 
 Homebrew (might) be still on 1.1.0, so if on Mac you might need to build it
-yourself also. OpenMP isn't available for clang, so you can either install gcc:
+yourself also. OpenMP isn't available for clang, so you can either install gcc::
 
-```
-brew tap homebrew/versions
-brew install gcc48
-```
+  brew tap homebrew/versions
+  brew install gcc48
 
 or just install without it. Just know that `num_threads` won't do anything.
 
-## Usage
+Usage
+-----
+::
 
-```
-from clustalo import clustalo
-input = {
-    'seq1': 'AAATCGGAAA',
-    'seq2': 'CGGA'
-}
-aligned = clustalo(input)
-# aligned is a dict of aligned sequences:
-#   seq1: AAATCGGAAA
-#   seq2: ----CGGA--
-```
+  from clustalo import clustalo
+  input = {
+      'seq1': 'AAATCGGAAA',
+      'seq2': 'CGGA'
+  }
+  aligned = clustalo(input)
+  # aligned is a dict of aligned sequences:
+  #   seq1: AAATCGGAAA
+  #   seq2: ----CGGA--
 
 At the moment, input sequences are assumed to not be aligned (i.e. there is no
-dealign option). See `clustaslo/__init__.py` for documentation.
+dealign option). See `clustalo.clustalo.__doc__` or file `clustaslo/clustalo.c`
+for documentation.
 
--- clustalo.c
+++ clustalo.c
@@ -122,21 +122,21 @@ clustalo_clustalo(PyObject *self, PyObject *args, PyObject *keywds)
 
 static PyMethodDef ClustaloMethods[] = {
     {"clustalo",  (PyCFunction)clustalo_clustalo, METH_VARARGS | METH_KEYWORDS,
-     "Runs clustal omega and returns a dictionary of sequence_named => aligned_bases ('_' for gaps)"
-     "If timeout is reached, returns None"
+     "Runs clustal omega.
      ""
-     "Arguments:"
-     "data -- dictionary of sequence_name => bases"
+     "Args:"
+     "  data (dict): dictionary of sequence_name => bases"
      ""
-     "Optional arguments:"
-     "timeout -- maximum time (seconds) before aborting"
-     "seqtype -- should be one of clustalo.DNA, clustalo.RNA, or clustalo.PROTEIN"
-     "mbed_guide_tree -- whether mBed-like clustering guide tree should be used"
-     "mbed_iteration -- whether mBed-like clustering iteration should be used"
-     "num_combined_iterations -- number of (combined guide-tree/HMM) iterations"
-     "max_guidetree_iterations -- max guide tree iterations within combined iterations"
-     "max_hmm_iterations -- max HMM iterations within combined iterations"
-     "num_threads -- number of threads to use (requires libclustalo compiled with OpenMP)"},
+     "Kwargs::"
+     "  seqtype (int): should be one of clustalo.DNA, clustalo.RNA, or clustalo.PROTEIN"
+     "  mbed_guide_tree (bool): whether mBed-like clustering guide tree should be used"
+     "  mbed_iteration (bool): whether mBed-like clustering iteration should be used"
+     "  num_combined_iterations (int): number of (combined guide-tree/HMM) iterations"
+     "  max_guidetree_iterations (int): max guide tree iterations within combined iterations"
+     "  max_hmm_iterations (int): max HMM iterations within combined iterations"
+     "  num_threads (int): number of threads to use (requires libclustalo compiled with OpenMP)"},
+     ""
+     "Returns dict of sequence_named => aligned_bases ('_' for gaps)"
     {NULL, NULL, 0, NULL}
 };
 
