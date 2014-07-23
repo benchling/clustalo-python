a4e64477e4d7381e61341fb5eb1fe95023232cda
Sherwin Yu <sherwinxyu@gmail.com>
2014-07-23 16:11:53 -0700

    Add OPENMP_DISABLED support to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from distutils.core import setup, Extension
+import os
+
+OPENMP_DISABLED = os.environ.get('OPENMP_DISABLED', False)
+libraries=['clustalo', 'stdc++']
+extra_compile_args = []
+
+if not OPENMP_DISABLED:
+    libraries.append('gomp')
+    extra_compile_args.append('-fopenmp')
 
 module = Extension('clustalo',
                    sources = ['clustalo.c'],
                    include_dirs=['/usr/include/clustalo', '/usr/local/include/clustalo'],
-                   libraries=['clustalo', 'stdc++', 'gomp'],
-                   extra_compile_args=['-fopenmp'])
+                   libraries=libraries,
+                   extra_compile_args=extra_compile_args)
 
 setup(name='clustalo',
       version='0.1',
